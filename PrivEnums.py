a588f5c6cc4b6b6b313f8abde7c8f30525215cfd
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-05-28 01:24:47 +0100

    foundation for new privilege system

---
+++ PrivEnums.py
@@ -0,0 +1,14 @@
+# ok so here are the enums for the new priv system
+# rather than what cvolton does now, GDPyS will use bitwise privileges similar to RealistikOsu
+
+LogIn = 2 << 0
+PostAccComment =  2 << 1
+PostComment = 2<<2
+UploadLevel = 2 << 3
+ModRateLevel = 2 << 4
+ModRateDemon = 2 << 5
+ModRegularBadge = 2 << 6
+ModElderBadge = 2 << 7
+ClaimDailyChest = 2 << 8
+CommandSetAcc = 2 << 9
+CommandRename = 2 << 10

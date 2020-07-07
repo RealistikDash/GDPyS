b14fe3b95d4a3c6a9a0ef4e228986d34b02f901a
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-07-07 18:05:08 +0100

    UpdateUserScore has to return userID

--- functions.py
+++ functions.py
@@ -314,7 +314,7 @@ def UpdateUserScore(request):
         )
     mydb.commit()
     Success(f"Successfully updated the user profile for {request.form['userName']}!")
-    return "1"
+    return UserID
 
 def JointStringBuilder(Content: dict):
     """Builds a joint string out of a dict."""

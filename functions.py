d6d2734221a84fa291751a451c691dfca453da5f
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-07-22 21:50:18 +0100

    prepare login handler

--- functions.py
+++ functions.py
@@ -2196,4 +2196,36 @@ def GetFriendReqList(request):
         }) + "|"
     
     return f"{ReturnStr[:-1]}#{Count}:{Offset}:10"
+
+# TODO: MOVE TO OWN FILE
+def ToolLoginCheck(request) -> bool:
+    """Handles the login checks for user tools. Returns new session if true"""
+    #Return struct is (T/F based on result, Message)
+    Username = request.form["username"]
+    Password = request.form["password"]
+
+    mycursor.execute("SELECT userName, password, accountID, Privileges FROM accounts WHERE userName LIKE %s LIMIT 1", (Username,))
+    User = mycursor.fetchone()
+    if User == None:
+        return (False, "User not found!")
+    
+    UserID = AIDToUID(User[2])
+    #check if not banned
+    mycursor.execute("SELECT isBanned FROM users WHERE userID = %s LIMIT 1", (UserID,))
+    if mycursor.fetchone()[0]:
+        return (False, "You are banned!")
+    
+    if not HasPrivilege(User[2], UserLogIn):
+        return (False, "You do not have permission to log in!")
+
+    # password check
+    if not CheckBcryptPw(User[1], Password):
+        return (False, "Incorrect password!")
+    
+    return (True, {
+        "AccountID" : User[2],
+        "Username" : User[0],
+        "Privileges" : User[3],
+        "LoggedIn" : True
+    })
     

eff79a433ec136c01aacc03bdc9af9412831cefd
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-07-28 12:50:30 -0700

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,16 +1,18 @@
 import threading
 import os
     
+dir_path = os.path.dirname(os.path.realpath(__file__))
+   
 def add_plugins():
     def exec_plugin(code, name):
         exec(code)
         print(f"Loaded plugin {name}!")
 
-    if not os.path.exists("plugins"):
-        os.mkdir("plugins")
-    for plugin in os.listdir("plugins"):
-        if os.path.isdir("plugins/" + plugin):
+    if not os.path.exists(dir_path + "/plugins"):
+        os.mkdir(dir_path + "/plugins")
+    for plugin in os.listdir(dir_path + "/plugins"):
+        if os.path.isdir(dir_path + "/plugins/" + plugin):
             continue
         print(f"Loading plugin {plugin}")
-        f = open("plugins/" + plugin, "r")
+        f = open(dir_path + "/plugins/" + plugin, "r")
         threading.Thread(target=exec_plugin, args=[f.read(), plugin]).start()

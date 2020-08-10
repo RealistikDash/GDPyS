2ff8a6de3a41841909698a493069d3f9fa0b38f6
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-10 13:48:10 +0100

    modify the way reupload api sends things

--- static/js/GDPyS.js
+++ static/js/GDPyS.js
@@ -35,7 +35,7 @@ function IziFail(MainText, OtherText) {
 function ReuploadLevel(LevelID, server) {
     //Why did I make it like this??
     server = encodeURIComponent(server); //urlencode it
-    fetch("http://"+ location.host +`/api/reuploadapi/${LevelID}/${server}`)
+    fetch("http://"+ location.host +`/api/reuploadapi?levelid=${LevelID}&server=${server}`)
 	.then(res => res.json())
 	.then((out) => {
         if (out["status"] == 404) {

5e49c22bd2a04060cc01db1ffab73551b698e73c
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-17 15:58:24 +0100

    forgot to pass levelid

--- functions.py
+++ functions.py
@@ -2329,6 +2329,6 @@ def RateDemonHandler(request):
     DemonType = {
         1:3,2:4,3:0,4:5,5:6
     }[int(request.form["rating"])]
-    mycursor.execute("UPDATE levels SET starDemonDiff = %s WHERE levelID = %s LIMIT 1", (DemonType,))
+    mycursor.execute("UPDATE levels SET starDemonDiff = %s WHERE levelID = %s LIMIT 1", (DemonType, int(request.form["levelID"])))
     mydb.commit()
     return "1"

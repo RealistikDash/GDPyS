68fe61a58eaa97ba5a31212d11d8c6ac96c5b724
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-29 21:01:31 +0100

    finish level struct

--- helpers/generalhelper.py
+++ helpers/generalhelper.py
@@ -1,4 +1,10 @@
-#simple functions with the aim of automating data-related tasks
+#simple misc functions thaat aim to replace 
+import time
+
 def dict_keys(dictioary: dict) -> tuple:
     """Returns a tuple of all the dictionary keys."""
     return tuple(dictioary.keys())
+
+def timestamp() -> int:
+    """Gets current timestamp as an int."""
+    return round(time.time())

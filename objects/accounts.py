de4bcbe1c824db8cce383fa2b66efc1c33181fb1
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-29 21:13:06 +0100

    account struct

---
+++ objects/accounts.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+@dataclass
+class Account():
+    """Account struct."""
+    username : str
+    email : str
+    account_id : int
+    user_id : int
+    register_date : int
+    privileges : int
+    stars : int
+    icon : int
+    colour1 : int
+    colour2 : int
+    icon_type : int
+    coins : int
+    user_coins : int
+    ship : int
+    ball : int
+    ufo : int
+    wave : int
+    robot : int
+    glow : bool
+    cp : int
+    diamonds : int
+    orbs : int
+    spider : int
+    explosion : int
+    banned : bool

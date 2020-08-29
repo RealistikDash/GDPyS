72d04bd80ac64b93046cc5c3cafa9f0bc75368d6
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-29 21:01:31 +0100

    finish level struct

--- objects/levels.py
+++ objects/levels.py
@@ -3,7 +3,29 @@ from dataclasses import dataclass
 @dataclass
 class Level():
     game_version : int
-    level_id : int
+    ID : int
     name : str
     description : str
     version : int
+    length : int
+    track : int
+    password : int
+    original : bool
+    two_player : bool
+    song_id : bool
+    objects : int
+    coins : int
+    requested_stars : int
+    string : str
+    info : str
+    extra : str
+    stars : int
+    upload_timestamp : int
+    update_timestamp : int
+    verified_coins : bool
+    featured: bool
+    epic : bool
+    demon_diff : int
+    user_id : int
+    account_id : int
+    ldm : bool

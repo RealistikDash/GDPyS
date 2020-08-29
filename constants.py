66de430b174a0457c21c8b94e61d81d5b71afd23
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-29 17:39:44 +0100

    expand crypt helper and store xor keys in constant

---
+++ constants.py
@@ -0,0 +1,5 @@
+class XorKeys():
+    password = 37526
+    chk = 19847
+    level_password = 26364
+    quests = 19847
--- helpers/crypthelper.py
+++ helpers/crypthelper.py
@@ -3,16 +3,18 @@ import base64
 import hashlib
 import string
 import random
+from itertools import cycle
+from constants import XorKeys
 
 def hash_sha1(text: str) -> str:
     """Hashes text in SHA1."""
     return hashlib.sha1(text.encode()).hexdigest()
 
-def base64decode(text: str) -> str:
+def decode_base64(text: str) -> str:
     """Decodes base64."""
     return base64.b64decode(text.encode()).decode()
 
-def base64encode(text: str) -> str:
+def encode_base64(text: str) -> str:
     """Encodes base64."""
     return base64.b64encode(text.encode()).decode()
 
@@ -29,6 +31,19 @@ def compare_bcrypt(text1: str, text2: str) -> bool:
         pass
     return check
 
-def random_string(length = 8):
+def generate_random_string(length = 8):
     """Generates a random string made of upper and lower case ascii characters of given length."""
     return "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(length))
+
+
+def cipher_xor(data: str, key: str):
+    xor = "".join(chr(ord(x) ^ ord(y)) for (x,y) in zip(str(data), cycle(str(key))))
+    return xor
+
+def decode_gjp(text: str) -> str:
+    """Decodes GJP (Geometry Dash's password encryption method) and returns in plaintext."""
+    return cipher_xor(decode_base64(text), XorKeys.password)
+
+def encode_chk(text: str) -> str:
+    """Encodes a Geometry Dash chunk."""
+    return cipher_xor(encode_base64(text[5:]), XorKeys.chk)

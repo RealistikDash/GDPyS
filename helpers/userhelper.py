1e3da1b171f56401bc52061df50ad7b62daa49a3
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-08-29 22:28:47 +0100

    create a proper user helper

---
+++ helpers/userhelper.py
@@ -0,0 +1,76 @@
+from helpers.auth import auth
+from helpers.generalhelper import dict_keys
+from helpers.crypthelper import decode_base64
+from objects.accounts import Account
+from objects.comments import AccountComment
+from conn.mysql import myconn
+
+class UserHelper():
+    """Responsible for caching and getting user objects and other user-related actions."""
+    def __init__(self):
+        self.object_cache = {}
+        self.accid_userid_cache = {}
+    
+    async def _create_user_object(self, account_id: int) -> Account:
+        """Creates a user object."""
+        account_id = int(account_id)
+        user_id = await self.accid_userid(account_id)
+        async with myconn.conn.cursor() as mycursor:
+            mycursor.execute("SELECT userName, email, registerDate, privileves FROM accounts WHERE accountID = %s LIMIT 1", (account_id,))
+            account_data = await mycursor.fetchone()
+            mycursor.execute("SELECT stars,demons,icon,color1,color2,iconType, coins,userCoins,accShip,accBall,accBird,accDart,accRobot,accGlow,creatorPoints,diamonds,orbs,accSpider,accExplosion,isBanned FROM users WHERE extID = %s LIMIT 1", (account_id,))
+            user_data = await mycursor.fetchone()
+            mycursor.execute("SELECT userID, userName, comment, timestamp, likes, isSpam FROM acccoments WHERE userID = %s LIMIT 1", (user_id,))
+            comments = await mycursor.fetchall()
+        
+        acc_comments = []
+
+        for comment in comments:
+            acc_comments.append(AccountComment(comment[0], decode_base64(comment[2]), comment[3], comment[4], bool(comment[5]), comment[1]))
+        
+        return Account(
+            account_data[0],
+            account_data[1],
+            account_id,
+            user_id,
+            account_data[2],
+            account_data[3],
+            user_data[0],
+            user_data[1],
+            user_data[2],
+            user_data[3],
+            user_data[4],
+            user_data[5],
+            user_data[6],
+            user_data[7],
+            user_data[8],
+            user_data[9],
+            user_data[10],
+            user_data[11],
+            user_data[12],
+            bool(user_data[13]),
+            user_data[14],
+            user_data[15],
+            user_data[16],
+            user_data[17],
+            user_data[18],
+            bool(user_data[19])
+        )
+    
+    async def _cache_aid_uid(self, account_id: int) -> None:
+        """Caches an account id to user id value."""
+        account_id =int(account_id)
+        async with myconn.conn.cursor() as mycursor:
+            mycursor.execute("SELECT userID FROM users WHERE accountID = %s LIMIT 1", (account_id,))
+            user_id = await mycursor.fetchone()
+        assert user_id is not None
+        self.accid_userid_cache[account_id] = user_id[0]
+    
+    async def accid_userid(self, account_id : int) -> int:
+        """Returns the userID of a user with given accountID."""
+        account_id = int(account_id)
+        if account_id not in dict_keys(self.accid_userid_cache):
+            await self._cache_aid_uid(account_id)
+        return self.accid_userid_cache[account_id]
+
+user_helper = UserHelper() # This has to be a common class.

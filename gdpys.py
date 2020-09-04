edd0ef16c3724fc4136b3c80e9ad0cee6531ee0a
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-09-04 21:08:40 +0100

    WELCOME ASCII ART

--- gdpys.py
+++ gdpys.py
@@ -5,6 +5,7 @@ from handlers.frontend import home_page
 from handlers.login import login_handler
 from handlers.register import register_handler
 from config import user_config
+from constants import ASCII_ART
 from conn.mysql import create_connection
 
 def config_routes(app: web.Application) -> None:
@@ -12,6 +13,10 @@ def config_routes(app: web.Application) -> None:
     app.router.add_get("/", home_page)
     app.router.add_post("/database/accounts/loginGJAccount.php", login_handler)
     app.router.add_post("/database/accounts/registerGJAccount.php", register_handler)
+
+def welcome_sequence():
+    """Startup welcome print art things."""
+    print(ASCII_ART)
 
 async def init(loop):
     """Initialises the app and MySQL connection."""
@@ -23,6 +28,7 @@ if __name__ == "__main__":
     # Configures the logger.
     logging_level = logging.DEBUG if user_config["debug"] else logging.INFO
     logging.basicConfig(level = logging_level)
+    welcome_sequence()
     # Inits the app.
     loop = asyncio.get_event_loop()
     app = loop.run_until_complete(init(loop))

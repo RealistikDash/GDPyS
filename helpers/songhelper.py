72cc9155d14aca33e99250d8555d1e51d5f68930
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-09-07 19:43:12 +0100

    BRUH ITS ASYNC WITH GHUIEFDEDFGHUIGERHIU

--- helpers/songhelper.py
+++ helpers/songhelper.py
@@ -15,7 +15,7 @@ class SongHelper():
     
     async def _top_artists(self) -> list:
         """Returns a list of top artists names."""
-        with myconn.conn.cursor() as mycursor:
+        async with myconn.conn.cursor() as mycursor:
             await mycursor.execute("SELECT authorName AS frequency FROM songs GROUP BY authorName ORDER BY frequency DESC")
             authors = await mycursor.fetchall()
         

686f3cadcb8887b760cc62994349a0acf2f96ef4
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-09-28 06:47:42 -0700

    move commands into client

--- commands/levels.py
+++
@@ -1,6 +0,0 @@
-from exceptions import GDPySCommandError
-from objects.comments import CommandContext
-
-async def test_command(ctx : CommandContext):
-    """GDPyS test command."""
-    raise GDPySCommandError("Yes")

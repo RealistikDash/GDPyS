9f842065d300dd39bbbd23ced932da8ba33fb497
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-10-06 14:40:07 -0700

    finish lang helper

--- handlers/lang.py
+++ handlers/lang.py
@@ -2,12 +2,11 @@ import json
 import os
 
 class Lang:
-    def __init__(self):
+    def __init__(self, lang):
         for f in os.listdir("lang"):
-            if f.endswith(".json"):
+            if f == lang + ".json":
                 lang = json.load(open("lang/" + f, "r"))
                 for key, value in lang.items():
                     setattr(self, key, value)
-
 
 lang = Lang()

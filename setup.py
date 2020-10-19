c34e650f9f07059e4556c1d08842069a6333de05
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-10-19 12:49:29 -0700

    fix license in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description=(
         "GDPyS is a Python based Geometry Dash server, it has many unique features including plugins and custom magic and awarded sections. What makes GDPyS better than other alternatives is because of the speed. GDPyS runs much faster than other alternatives and offers many configuration options like Cheatless AntiCheat and more."
     ),
-    license="MIT",
+    license="GPLv3",
     packages=["gdpys"],
     long_description=read("README.md"),
     project_urls={

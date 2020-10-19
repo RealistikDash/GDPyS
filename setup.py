1a7cb1ebda7d022b15aafbb05142e0a28150a075
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-10-19 12:47:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,21 @@ setup(
     description=(
         "GDPyS is a Python based Geometry Dash server, it has many unique features including plugins and custom magic and awarded sections. What makes GDPyS better than other alternatives is because of the speed. GDPyS runs much faster than other alternatives and offers many configuration options like Cheatless AntiCheat and more."
     ),
+    license="MIT",
     packages=["gdpys"],
     long_description=read("README.md"),
+    project_urls={
+        "Documentation": "https://gdpys.readthedocs.io/en/latest",
+        "Issue tracker": "https://github.com/RealistikDash/GDPyS/issues",
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"gdpys": ["gdpys = gdpys.__main__:main"]},
 )

a05915582e33b0915e45880654a67f69928d9876
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-11-04 09:24:45 -0800

    bruh gaming

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="gdpys",
-    version=__version__,
+    version=version,
     author="RealistikDash",
     description=(
         "GDPyS is a Python based Geometry Dash server, it has many unique features including plugins and custom magic and awarded sections. What makes GDPyS better than other alternatives is because of the speed. GDPyS runs much faster than other alternatives and offers many configuration options like Cheatless AntiCheat and more."

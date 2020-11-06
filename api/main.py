719c88f04a33b273de52e482c3f23856e0646a79
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-11-06 11:35:07 -0800

    sorry nekit uwu

--- api/main.py
+++ api/main.py
@@ -14,75 +14,16 @@ def json_resp(*args, **kwargs) -> web.Response:
         actual_kwargs[key] = value
     return web.json_response(*args, **actual_kwargs)
 
-def parse_string(string: str): # uwu thanks nekit
-    if string is None:
-        return {}
-
-    current_section = ""
-    current_content = None
-    result = {}
-
-    lines = [line for line in string.strip().splitlines() if line] + [""]
-    result["method"], result["path"] = lines.pop(0).strip().split(maxsplit=1)
-
-    for line in lines:
-        line = line.strip(". ;")
-        if line.endswith(":") or not line:
-            if current_section:
-                if isinstance(current_content, list):
-                    current_content = "\n".join(current_content)
-
-                result[current_section] = current_content
-                current_content = None
-
-            current_section = line.lower().rstrip(":").replace(" ", "_")
-
-        else:
-            key, sep, value = line.partition(": ")
-
-            if sep:
-                if current_content is None:
-                    current_content = {}
-
-                current_content[key] = value
-
-            else:
-                if current_content is None:
-                    current_content = []
-
-                current_content.append(line)
-
-    return result
-
-def parse_route_docs():
-    for route in routes:
-        info = dict(name=route.handler.__name__)
-        info.update(parse_string(route.handler.__doc__))
-        yield info
+def text(**kwargs) -> aiohttp.web.Response:
+    return aiohttp.web.Response(**kwargs, content_type="text/html")
 
 async def home(request:web.Request):
-    payload = {
-        "aiohttp": aiohttp.__version__,
-        "python": platform.python_version(),
-        "routes": list(parse_route_docs()),
-    }
-    return json_resp(payload)
+    return web.HTTPOk()
 
 app.router.add_get("/", home)
 
 @routes.get("/user/{user}")
 async def user(request:aiohttp.web.Request):
-    """GET /api/user/{user}
-    Description:
-        Get a user by id
-    Example:
-        link: /api/user/2
-    Returns:
-        200: A user object
-        404: User not found
-    Return Type:
-        application/json
-    """
     user = request.match_info["user"]
     try: 
         int(user)
